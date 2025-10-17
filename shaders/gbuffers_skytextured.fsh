8ddb6029063f8ac359b066377acaa415c67dac5b
Benjamin Rowan <benrowan910@gmail.com>
2025-10-17 12:30:56 -0500

    Replaced Shaders folder with base V#330

--- shaders/gbuffers_skytextured.fsh
+++
@@ -1,2 +0,0 @@
-#version 460 compatibility
-#include "/programs/basic_frag.glsl"
--- shaders/programs/shadow_frag.glsl
+++
@@ -1,4 +0,0 @@
-#version 460 compatibility
-const int shadowMapResolution = 2048;
-
-#include "/programs/basic_frag.glsl"
